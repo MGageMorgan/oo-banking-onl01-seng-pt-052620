50f4de4862a0dd69f0884afdbce77730a152a939
Gage Morgan <gage@gages.blog>
2020-06-29 02:10:51 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,10 +1,11 @@
 class Transfer
   attr_reader :sender, :receiver
-  attr_accessor :value
+  attr_accessor :value, :status
 
   def initialize(sender, receiver, value)
     @sender = sender
     @receiver = receiver
     @value = value
+    @status = "pending"
   end
 end
