8f6b99cd518336f5bc8d714480ce90a384a7b817
Gage Morgan <gage@gages.blog>
2020-06-29 01:56:55 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,11 +1,12 @@
 class BankAccount
   attr_reader :account, :name
-  attr_accessor :balance
+  attr_accessor :balance, :status
 
   def initialize(account)
     @account = account
     @name = account
     @balance = 1000
+    @status = "open"
   end
 
 end
